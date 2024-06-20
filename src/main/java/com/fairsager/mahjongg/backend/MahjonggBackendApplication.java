38bb95e85329b34f50f6d8c740ca9facee10e18a
Timon Stronczek <stronczek.timon@gmail.com>
2024-06-20 23:03:06 +0200

    Initial commit

---
+++ src/main/java/com/fairsager/mahjongg/backend/MahjonggBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fairsager.mahjongg.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjonggBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MahjonggBackendApplication.class, args);
+    }
+
+}
