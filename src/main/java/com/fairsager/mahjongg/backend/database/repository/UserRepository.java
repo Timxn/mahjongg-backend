a48b464d7f3afedc1ae769f031c4d009d78d65bc
Timon Stronczek <stronczek.timon@gmail.com>
2024-06-23 01:55:10 +0200

    #1: made improvements to the code

--- src/main/java/com/fairsager/mahjongg/backend/database/repository/UserRepository.java
+++ src/main/java/com/fairsager/mahjongg/backend/database/repository/UserRepository.java
@@ -2,15 +2,15 @@ package com.fairsager.mahjongg.backend.database.repository;
 
 import com.fairsager.mahjongg.backend.database.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
 
     User findByUsername(String username);
-
-    User findByUserId(UUID userId);
 
     List<User> findByUsernameContainingOrDisplayNameContaining(String query, String query2);
 }
